388fc9d2bff4a87a5d1451d33fd64902d63f5ea5
indraneel-Actyv <indraneel@actyv.com>
2020-01-17 12:11:41 +0530

    added payload

--- test/alltest.test.js
+++ test/alltest.test.js
@@ -1,4 +1,5 @@
 const { registerVendor } = require("../methods/vendormethods");
+const {ActyvPro} =require("../model/vendormodel")
 const { addingProductToInventory } = require("../methods/productmethods");
 const { addBusinessType } = require("../methods/businessmethods");
 
@@ -9,12 +10,67 @@ before(done => {
     lastName: "ch",
     email: "abhishekch65@gmail.com",
     phone: "+918897626060",
-    contacts: [],
+    contacts: [{
+      customerId:"101",
+      customerName:"Indraneel",
+      customerAddress:"Adilabad",
+      subscription:[{
+        subscribeId: "10001",
+        subscribeFrom: "Thu Jan 1 2020 05:30:30 GMT+0530 (India Standard Time)",
+        subscribeTo: "Thu Jan 31 2020 05:30:30 GMT+0530 (India Standard Time)",
+        vacations: [
+          "Thu Jan 16 2020 05:30:30 GMT+0530 (India Standard Time)",
+          "Thu Jan 23 2020 05:30:30 GMT+0530 (India Standard Time)",
+          "Thu Jan 29 2020 05:30:30 GMT+0530 (India Standard Time)"
+        ],
+        subscribedOnWeekend: true,
+        productId: "1001"
+      }]
+    }],
     invoices: [],
-    BusinessTypes: []
-  };
-  registerVendor(payload);
-  done();
+    BusinessTypes: [
+    {
+        name: "Newspaper",
+        businessId: "News32",
+        inventory: [{
+          productId:"1001",
+          name:"Hindu",
+          description:"",
+          units:"units",
+          ProductModels:[
+            { model: "Sunday", price: "6" },
+            { model: "Monday", price: "6" },
+            { model: "Tuesday", price: "6" },
+            { model: "Wednesday", price: "6" },
+            { model: "Thursday", price: "6" },
+            { model: "Friday", price: "6" },
+            { model: "Saturday", price: "6" }
+          ],
+          customers:[
+            {
+              customerId:"101",
+              customerName:"Indraneel",
+              customerAddress:"Adilabad",
+              subscription:[{
+                subscribeId: "10001",
+                subscribeFrom: "Thu Jan 1 2020 05:30:30 GMT+0530 (India Standard Time)",
+                subscribeTo: "Thu Jan 31 2020 05:30:30 GMT+0530 (India Standard Time)",
+                vacations: [
+                  "Thu Jan 16 2020 05:30:30 GMT+0530 (India Standard Time)",
+                  "Thu Jan 23 2020 05:30:30 GMT+0530 (India Standard Time)",
+                  "Thu Jan 29 2020 05:30:30 GMT+0530 (India Standard Time)"
+                ],
+                subscribedOnWeekend: true,
+                productId: "1001"
+              }
+          ]
+        }]
+    }]
+  }]
+};
+registerVendor(payload);
+console.log(ActyvPro)
+done();
 });
 /**
  * @function
@@ -22,7 +78,7 @@ before(done => {
  * @param {string} description - string explaining what test should do
  * @param {callback} middleware - function with done as a param
  */
-describe("creating a Vendor", function() {
+describe("creating a Vendor", function(done) {
   it("should create a instance of vendor. returns  change in size of total_no_of_vendors.", function() {
     const payload = {
       firstName: "abhi",
@@ -36,7 +92,9 @@ describe("creating a Vendor", function() {
     no_of_vendors = registerVendor(payload);
     no_of_vendors.should.be.a("number");
     no_of_vendors.should.equal(1);
+    done();
   });
+
 });
 
 /**
@@ -45,10 +103,10 @@ describe("creating a Vendor", function() {
  * @param {string} description - string explaining what test should do
  * @param {callback} middleware - function with done as a param
  */
-describe("creating a Business", function() {
+describe("creating a Business", function(done) {
   it("should create a instance of Business. returns  change in size of no of businesses.", function() {
     const payload = {
-      phone: "+918897626060",
+      phone: "+917207735192",
       businessType: {
         name: "News paper",
         businessId: "News32",
@@ -58,6 +116,7 @@ describe("creating a Business", function() {
     no_of_businesses_added = addBusinessType(payload);
     no_of_businesses_added[0].should.be.a("number");
     no_of_businesses_added[0].should.equal(1);
+    done();
   });
 });
 
@@ -67,9 +126,9 @@ describe("creating a Business", function() {
  * @param {string} description - string explaining what test should do
  * @param {callback} middleware - function with done as a param
  */
-describe("Adding product to Inventory of Vendor", function() {
+describe("Adding product to Inventory of Vendor", function(done) {
   it("should create a instance of Product. returns change in size of Inventory of Vendor.", function() {
-    let phone = "+918897626060";
+    let phone = "+917207735192";
     let businessPayload = {
       name: "News paper",
       businessId: "News32",
@@ -84,8 +143,13 @@ describe("Adding product to Inventory of Vendor", function() {
       { model: "Friday", price: "6" },
       { model: "Saturday", price: "6" }
     ];
-    no_of_Products_added=addingProductToInventory(phone, businessPayload, productPayload);//change in no of products
+    no_of_Products_added = addingProductToInventory(
+      phone,
+      businessPayload,
+      productPayload
+    ); //change in no of products
     no_of_Products_added[0].should.be.a("number");
     no_of_Products_added[0].should.equal(1);
+    done();
   });
 });
