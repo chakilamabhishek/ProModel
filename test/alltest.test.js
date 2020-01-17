d3ea73c144be07fd2ad3bd3642613a79526501db
abhishekchakilam <abhishek@actyv.com>
2020-01-17 18:40:50 +0530

    added object modelling

--- test/alltest.test.js
+++ test/alltest.test.js
@@ -1,11 +1,15 @@
 const { registerVendor } = require("../methods/vendormethods");
-const { addingProductToInventory, addingProductModelToProduct } = require("../methods/productmethods");
+const {
+  addingProductToInventory,
+  addingProductModelToProduct
+} = require("../methods/productmethods");
 const { addBusinessType } = require("../methods/businessmethods");
 const { newpayload } = require("../sampedata");
 const { calculateInvoice } = require("../methods/invoicemethods");
 const { addCustomer, addSubscription } = require("../methods/customerMethods");
 
 const should = require("chai").should();
+
 before(done => {
   const payload = {
     firstName: "Abhishek",
@@ -81,11 +85,11 @@ describe("Adding product to Inventory of Vendor", function() {
       inventory: []
     };
     let productPayload = {
-      productId:"101",
-      name:"Hindu",
-      description:"This is Hindu Paper",
-      units:"units",
-      ProductModels : [
+      productId: "101",
+      name: "Hindu",
+      description: "This is Hindu Paper",
+      units: "units",
+      ProductModels: [
         { model: "Sunday", price: "6" },
         { model: "Monday", price: "6" },
         { model: "Tuesday", price: "6" },
@@ -100,7 +104,7 @@ describe("Adding product to Inventory of Vendor", function() {
       phone,
       businessPayload,
       productPayload
-    ); 
+    );
     no_of_Products_added[0].should.be.a("number");
     no_of_Products_added[0].should.equal(1);
   });
@@ -115,10 +119,9 @@ describe("Adding product to Inventory of Vendor", function() {
 describe("Adding productModel to Product of Vendor's Inventory", function() {
   it("should create a instance of ProductModel. returns change in size of ProductModels in PRODUCT of Vendor's Inventory.", function() {
     let phone = "+919876543210";
-    let businessname= "News paper";
-    let  productname="Hindu";
-    let productModelPayload = 
-      { model: "Saturday", price: "6" }
+    let businessname = "News paper";
+    let productname = "Hindu";
+    let productModelPayload = { model: "Saturday", price: "6" };
     no_of_Products_added = addingProductModelToProduct(
       phone,
       businessname,
@@ -136,14 +139,46 @@ describe("Adding productModel to Product of Vendor's Inventory", function() {
  * @param {string} description - string explaining what test should do
  * @param {callback} middleware - function with done as a param
  */
-describe("Invoice ", function() {
-  it("it should calculate the invoice amount", function() {
-    amount = calculateInvoice(
-      newpayload.contacts[0].subscription[0],
-      newpayload.BusinessTypes[0].inventory[0]
+describe("Contact ", function() {
+  it("should create a instance of Product. returns change in size of Inventory of Vendor", function() {
+    let Customerinfo = {
+      customerId: "1023",
+      customerName: "Acabhishek",
+      customerAddress: "Hyderabad",
+      subscriptions: []
+    };
+    let vendorPhone = "+919876543210";
+    no_of_Customers_added = addCustomer(vendorPhone, Customerinfo);
+    no_of_Customers_added[0].should.be.a("number");
+    no_of_Customers_added[0].should.equal(1);
+  });
+});
+
+/**
+ * @function
+ * @inner
+ * @param {string} description - string explaining what test should do
+ * @param {callback} middleware - function with done as a param
+ */
+describe("Subscription ", function() {
+  it("should create a instance of Subscription. returns change in size of no of subscriptions", function() {
+    let subscriptoninfo = {
+      subscribeId: "1023",
+      subscribeFrom: "Thu Jan 1 2020 05:30:30 GMT+0530 (India Standard Time)",
+      subscribeTo: "Thu Jan 31 2020 05:30:30 GMT+0530 (India Standard Time)",
+      vacations: ["Thu Jan 29 2020 05:30:30 GMT+0530 (India Standard Time)"],
+      subscribedOnWeekend: true,
+      productId: "1001"
+    };
+    let customerName = "Acabhishek";
+    let vendorPhone = "+919876543210";
+    no_of_subscriptons_added = addSubscription(
+      vendorPhone,
+      customerName,
+      subscriptoninfo
     );
-    amount.should.be.a("number");
-    amount.should.equal(184);
+    no_of_subscriptons_added[0].should.be.a("number");
+    no_of_subscriptons_added[0].should.equal(1);
   });
 });
 
@@ -154,7 +189,15 @@ describe("Invoice ", function() {
  * @param {callback} middleware - function with done as a param
  */
 describe("Invoice ", function() {
-  it("it should calculate the invoice amount", function() {
+  it("it should calculate the invoice amount when user is on some vacations", function() {
+    amount = calculateInvoice(
+      newpayload.contacts[0].subscription[0],
+      newpayload.BusinessTypes[0].inventory[0]
+    );
+    amount.should.be.a("number");
+    amount.should.equal(184);
+  });
+  it("it should calculate the invoice amount when user Subscribed whole month", function() {
     let subscriptioninfo = {
       subscribeId: "10001",
       subscribeFrom: "Thu Jan 1 2020 05:30:30 GMT+0530 (India Standard Time)",
@@ -171,7 +214,7 @@ describe("Invoice ", function() {
     amount.should.equal(202);
   });
 
-  it("it should calculate the invoice amount", function() {
+  it("it should calculate the invoice amount when user is on some vacations and not subscribed on weekends", function() {
     let subscriptioninfo = {
       subscribeId: "10001",
       subscribeFrom: "Thu Jan 1 2020 05:30:30 GMT+0530 (India Standard Time)",
@@ -192,7 +235,7 @@ describe("Invoice ", function() {
     amount.should.equal(120);
   });
 
-  it("it should calculate the invoice amount", function() {
+  it("it should calculate the invoice amount when user is on some vacations and subscribed on weekends", function() {
     let subscriptioninfo = {
       subscribeId: "10001",
       subscribeFrom: "Thu Jan 1 2020 05:30:30 GMT+0530 (India Standard Time)",
@@ -240,54 +283,3 @@ describe("Invoice ", function() {
     amount.should.equal(132);
   });
 });
-
-/**
- * @function
- * @inner
- * @param {string} description - string explaining what test should do
- * @param {callback} middleware - function with done as a param
- */
-describe("Contact ", function() {
-  it("should create a instance of Product. returns change in size of Inventory of Vendor", function() {
-    let Customerinfo = {
-      customerId: "1023",
-      customerName: "Acabhishek",
-      customerAddress: "Hyderabad",
-      subscriptions: []
-    };
-    let vendorPhone = "+919876543210";
-    no_of_Customers_added = addCustomer(vendorPhone, Customerinfo);
-    no_of_Customers_added[0].should.be.a("number");
-    no_of_Customers_added[0].should.equal(1);
-  });
-});
-
-/**
- * @function
- * @inner
- * @param {string} description - string explaining what test should do
- * @param {callback} middleware - function with done as a param
- */
-describe("Subscription ", function() {
-  it("should create a instance of Product. returns change in size of Inventory of Vendor", function() {
-    let subscriptoninfo = {
-      subscribeId: "1023",
-      subscribeFrom: "Thu Jan 1 2020 05:30:30 GMT+0530 (India Standard Time)",
-      subscribeTo: "Thu Jan 31 2020 05:30:30 GMT+0530 (India Standard Time)",
-      vacations: ["Thu Jan 29 2020 05:30:30 GMT+0530 (India Standard Time)"],
-      subscribedOnWeekend: true,
-      productId: "1001"
-    };
-    let customerName = "Acabhishek";
-    let vendorPhone = "+919876543210";
-    no_of_subscriptons_added = addSubscription(
-      vendorPhone,
-      customerName,
-      subscriptoninfo
-    );
-    no_of_subscriptons_added[0].should.be.a("number");
-    no_of_subscriptons_added[0].should.equal(1);
-  });
-});
-
-
