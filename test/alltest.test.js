c475d02ddcd19c26c378ea1bc1ec23af9defb7ad
abhishekchakilam <abhishek@actyv.com>
2020-01-17 12:24:29 +0530

    resolved errors

--- test/alltest.test.js
+++ test/alltest.test.js
@@ -1,11 +1,21 @@
 const { registerVendor } = require("../methods/vendormethods");
-const {ActyvPro} =require("../model/vendormodel")
 const { addingProductToInventory } = require("../methods/productmethods");
 const { addBusinessType } = require("../methods/businessmethods");
+const { ActyvPro } =require("../model/vendormodel")
 
 const should = require("chai").should();
 before(done => {
   const payload = {
+    firstName: "Abhishek",
+    lastName: "ch",
+    email: "abhishekch65@gmail.com",
+    phone: "+918897626060",
+    contacts: [],
+    invoices: [],
+    BusinessTypes: []
+  };
+  registerVendor(payload);
+  let newpayload = {
     firstName: "Abhishek",
     lastName: "ch",
     email: "abhishekch65@gmail.com",
@@ -68,9 +78,8 @@ before(done => {
     }]
   }]
 };
-registerVendor(payload);
-console.log(ActyvPro)
-done();
+registerVendor(newpayload);
+  done();
 });
 /**
  * @function
@@ -78,7 +87,7 @@ done();
  * @param {string} description - string explaining what test should do
  * @param {callback} middleware - function with done as a param
  */
-describe("creating a Vendor", function(done) {
+describe("creating a Vendor", function() {
   it("should create a instance of vendor. returns  change in size of total_no_of_vendors.", function() {
     const payload = {
       firstName: "abhi",
@@ -92,9 +101,7 @@ describe("creating a Vendor", function(done) {
     no_of_vendors = registerVendor(payload);
     no_of_vendors.should.be.a("number");
     no_of_vendors.should.equal(1);
-    done();
   });
-
 });
 
 /**
@@ -103,10 +110,10 @@ describe("creating a Vendor", function(done) {
  * @param {string} description - string explaining what test should do
  * @param {callback} middleware - function with done as a param
  */
-describe("creating a Business", function(done) {
+describe("creating a Business", function() {
   it("should create a instance of Business. returns  change in size of no of businesses.", function() {
     const payload = {
-      phone: "+917207735192",
+      phone: "+918897626060",
       businessType: {
         name: "News paper",
         businessId: "News32",
@@ -116,7 +123,6 @@ describe("creating a Business", function(done) {
     no_of_businesses_added = addBusinessType(payload);
     no_of_businesses_added[0].should.be.a("number");
     no_of_businesses_added[0].should.equal(1);
-    done();
   });
 });
 
@@ -126,9 +132,9 @@ describe("creating a Business", function(done) {
  * @param {string} description - string explaining what test should do
  * @param {callback} middleware - function with done as a param
  */
-describe("Adding product to Inventory of Vendor", function(done) {
+describe("Adding product to Inventory of Vendor", function() {
   it("should create a instance of Product. returns change in size of Inventory of Vendor.", function() {
-    let phone = "+917207735192";
+    let phone = "+918897626060";
     let businessPayload = {
       name: "News paper",
       businessId: "News32",
@@ -143,46 +149,8 @@ describe("Adding product to Inventory of Vendor", function(done) {
       { model: "Friday", price: "6" },
       { model: "Saturday", price: "6" }
     ];
-    no_of_Products_added = addingProductToInventory(
-      phone,
-      businessPayload,
-      productPayload
-    ); //change in no of products
+    no_of_Products_added=addingProductToInventory(phone, businessPayload, productPayload);//change in no of products
     no_of_Products_added[0].should.be.a("number");
     no_of_Products_added[0].should.equal(1);
-  });
-});
-
-/**
- * @function
- * @inner
- * @param {string} description - string explaining what test should do
- * @param {callback} middleware - function with done as a param
- */
-describe("Adding addingProductModelToProduct", function() {
-  it("should create a instance of Product. returns change in size of Inventory of Vendor.", function() {
-    let phone = "+918897626060";
-    let businessPayload = {
-      name: "News paper",
-      businessId: "News32",
-      inventory: []
-    };
-    let productPayload = [
-      { model: "Sunday",price: "6"},
-      { model: "Monday", price: "6" },
-      { model: "Tuesday", price: "6" },
-      { model: "Wednesday", price: "6" },
-      { model: "Thursday", price: "6" },
-      { model: "Friday", price: "6" },
-      { model: "Saturday", price: "6" }
-    ];
-    no_of_Products_added = addingProductToInventory(
-      phone,
-      businessPayload,
-      productPayload
-    );
-    no_of_Products_added[0].should.be.a("number");
-    no_of_Products_added[0].should.equal(1);
-    done();
   });
 });
